2b88c3ae5ad0f0fea4b4275fbf338d1ae04f6116
K <K@DIEGO>
2017-09-09 17:59:09 -0300

    a

---
+++ LargestPrimeFactor/LargestPrimeFactor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargestPrimeFactor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (long i = 2; i < 6857; i++)
+            {
+                if (600851475143 % i == 0)
+                {
+                    Console.WriteLine(i);
+                    Console.ReadKey();
+                }
+
+            }
+
+
+
+            }
+        }
+    }
+
+
